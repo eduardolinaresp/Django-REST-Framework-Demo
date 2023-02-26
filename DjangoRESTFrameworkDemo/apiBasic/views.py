ecbcb50c8a3fc3df433977a03c47d83856b4c67c
Eduardo Linares <ing@bbcc.cl>
2023-02-26 15:18:10 -0300

    ViewSet & Routers

--- DjangoRESTFrameworkDemo/apiBasic/views.py
+++ DjangoRESTFrameworkDemo/apiBasic/views.py
@@ -10,6 +10,8 @@ from rest_framework  import status
 from rest_framework.views import APIView
 from rest_framework import generics
 from rest_framework import mixins
+from rest_framework.authentication import SessionAuthentication,TokenAuthentication,BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 
@@ -20,9 +22,12 @@ class GenericAPIView(generics.GenericAPIView,mixins.ListModelMixin,mixins.Create
       
       lookup_field = 'id'
       
+      authentication_classes = [SessionAuthentication, BasicAuthentication]
+      permission_classes = [IsAuthenticated]
+      
       def get(self, request, id =None):
           if id:
-             return self.retrive(request)
+             return self.retrieve(request)
           else:    
             return self.list(request)
       
